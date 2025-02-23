6fe8739f4be9f41c70a8613a74b0806938d606da
e1d0 <e1d0@h3x.sh>
2025-02-23 03:35:11 +0100

    update script

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -8,7 +8,7 @@ npm version "${VERSION}" --no-git-tag-version --force
 git add package.json package-lock.json
 git commit -m "Bump version to ${VERSION}"
 
-git tag "v${VERSION}" -m "Bump version to ${VERSION}"
+git tag "v${VERSION}" -m "Release version ${VERSION}"
 
 git push origin HEAD
 git push origin "refs/tags/v${VERSION}"
