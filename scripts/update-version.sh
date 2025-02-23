118fe7c9b6e3205ff7e7bf2d35b842bca7376e0d
e1d0 <e1d0@h3x.sh>
2025-02-23 04:10:45 +0100

    wip?: update-version-script

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -3,11 +3,7 @@
 TIMESTAMP=$(date +%s)
 VERSION="0.1.${TIMESTAMP}"
 
-npm version "${VERSION}" --no-git-tag-version --force
-
-git add package.json package-lock.json
-
-git tag "v${VERSION}" -m "Release version ${VERSION}"
+npm version "${VERSION}"
 
 git push origin HEAD
 git push origin "refs/tags/v${VERSION}"
